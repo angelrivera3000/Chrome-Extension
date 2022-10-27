be7841ca385b6870fa6c041c3fe050cb8258d825
angelrivera3000 <angelrivera3000@gmail.com>
2022-10-26 23:10:43 -0500

    Api Consumer

---
+++ script.js
@@ -0,0 +1,19 @@
+async function fetchData() {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+            'X-RapidAPI-Host': 'concerts-artists-events-tracker.p.rapidapi.com'
+        }
+    };
+    
+    const res = await fetch('https://concerts-artists-events-tracker.p.rapidapi.com/location?name=Toronto&minDate=2022-11-01&maxDate=2022-12-31&page=1', options)
+    const record = await res.json()
+    console.log('record', record)
+        /*
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+        */
+}
+fetchData();
